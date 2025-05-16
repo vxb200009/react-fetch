cd52732ccb328dbeef9b2860555226edb40f7196
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-15 20:42:46 -0500

    update postList

---
+++ src/components/PostList.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+const PostList: React.FC = () => {
+  return (
+  );
+};
+export default PostList;
