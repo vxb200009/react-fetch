edd6289c9537f2d1d3d78823458afb5f9ae41d6f
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-16 21:36:45 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PostList from './components/PostList';
 import ProductList from './components/ProductList';
+import PokemonList from './components/PokemonList';
 
 function App() {
   return (
     <div className="App">
       {/* <PostList /> */}
-      <ProductList />
+      {/* <ProductList /> */}
+      <PokemonList />
     </div>
   );
 }
