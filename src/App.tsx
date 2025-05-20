301dd27ac6007c98a80326a8d161115bb0faebf7
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-20 11:49:58 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import PostList from './components/PostList';
 import ProductList from './components/ProductList';
 import PokemonList from './components/PokemonList';
+import PostListWithPagination from './components/PostListWithPagination';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* <PostList /> */}
-      {/* <ProductList /> */}
-      <PokemonList />
+    <div className="min-h-screen bg-gray-50">
+      <main className="container mx-auto py-8">
+        <PostListWithPagination />
+        
+        {/* Other components can be uncommented and used as needed */}
+        {/* <PostList /> */}
+        {/* <ProductList /> */}
+        {/* <PokemonList /> */}
+      </main>
     </div>
   );
 }
